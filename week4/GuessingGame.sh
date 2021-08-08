124ef33bbca14e542698d4ceea99095ef84c0e08
jeffroz7 <jeff_roz@hotmail.com>
2021-08-08 14:51:39 +0800

    Improved script comments and saved output of sed command from Week 5.3 activity.

--- week4/GuessingGame.sh
+++ week4/GuessingGame.sh
@@ -11,6 +11,7 @@ printError()
 }
 
 # getNumber() function has been edited from the "GetNumber.sh" script in order to accomodate the different conditions required for the task.
+# The while loop below is active only if the number input by the user is not 42.S
 
 getNumber()
 
@@ -20,16 +21,19 @@ getNumber()
 
     while (( $REPLY != $4 )); do
 
+        # If the number input is less than 1 and greater than 100, return an error.
         if [[ $REPLY -lt $2 || $REPLY -gt $3 ]]; then
 
             printError "Input must be between $2 and $3"
             read -p "$1: "
 
+        # If the number is less than 42, we let the user know this by printing "Too Low!""
         elif [[ $REPLY -lt $4 ]]; then
 
             printError "Too low!"
             read -p "$1: "
-        
+
+        # If the number is greater than 42, we let the user know this by printing "Too High!"
         elif [[ $REPLY -gt $4 ]]; then 
 
             printError "Too High!"
@@ -41,8 +45,12 @@ getNumber()
 
 }
 
+# If the number does not meet any of the above situations, that is the number input is 42, then the while loop closes and we pass through to the below input.
+echo "Let's play a game"
 
-echo "Guess the number!"
+#echo "Guess the number!" 
+# An alternative welcoming message for the user. Changed to line 49 to work with Week 5.3 activities.
+
 getNumber "Choose a number between 1 and 100" 1 100 42
-echo -e $'\033[1;36m'Correct!$'\033[0m'
-#echo "Correct!"
+#echo -e $'\033[1;36m'Correct!$'\033[0m' - Prints "Correct" in cyan.
+echo "Right!"

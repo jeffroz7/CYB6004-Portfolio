e15c1bb1b388415be8f039890b277cfc07c877f5
jeffroz7 <jeff_roz@hotmail.com>
2021-08-23 12:32:50 +0800

    Improved passwordCheck.sh script comments

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# The -p flag allows the user to type on the same line as read.
+# The -s flag hides the user input.
 read -sp "password: " pass
 
 echo
 
-# creates a variable that is the hash of $pass
+# Creates a variable that is the hash of $pass
 check_pass=$( echo $pass | sha256sum )
 
-# assigns a variable to the contents of the secret.txt file, which is the hash of the password that is set by the user.
+# Assigns a variable to the contents of the secret.txt file, which is the hash of the password that is set by the user.
 set_pass=$( cat secret.txt )
 
-# a simple if statement that sees whether the hash of the input password matches the hash of the password set by the user.
-# if so, the user is granted access.
-# if not, the user is denied access.
+# A simple if statement that sees whether the hash of the input password matches the hash of the password set by the user.
+# If so, the user is granted access and the exit code is 0 (successful).
+# If not, the user is denied access and the exit code is 1, which is the exit code that we have set as failed.
 if [[ $check_pass == $set_pass ]]; then
 	echo "Access granted."
 	exit 0
